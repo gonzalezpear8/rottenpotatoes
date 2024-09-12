270a04e3f89e9c405237cb1c97e142d809326146
Ryan Gonzalez <gonzalezpear@tamu.edu>
2024-09-11 19:08:56 -0500

    made styling changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def sortable(column, title = nil)
+        title ||= column.titleize
+        direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+        link_to title, { sort: column, direction: direction }, { class: "sortable #{'current' if column == params[:sort]} #{direction}" }
+    end    
 end
